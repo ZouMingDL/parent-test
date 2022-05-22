1b27d540293fb0c0f7b26e6075a1231b66ea4786
ZouMing <714001035@qq.com>
2022-05-22 14:00:58 +0800

    添加学生参数

--- transl-test/src/main/java/com/trans/entity/Student.java
+++ transl-test/src/main/java/com/trans/entity/Student.java
@@ -1,5 +1,20 @@
 package com.trans.entity;
 
-public class Student {
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * @author ZouMing
+ */
+@Data
+public class Student implements Serializable {
+
     private Integer id;
+
+    private String name;
+
+    private Integer age;
+
+    private Integer classId;
 }
