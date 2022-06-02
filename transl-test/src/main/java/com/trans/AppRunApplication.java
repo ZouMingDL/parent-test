bb7e386f28121820fb636cbcbdd4b335ff0b5def
成都_邹佳骏 <714001035@qq.com>
2022-06-02 10:39:22 +0800

    dd

--- transl-test/src/main/java/com/trans/AppRunApplication.java
+++ transl-test/src/main/java/com/trans/AppRunApplication.java
@@ -1,15 +1,30 @@
 package com.trans;
 
+import com.trans.entity.Student;
+import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import java.util.ArrayList;
+
 @SpringBootApplication
 @EnableTransactionManagement
 @MapperScan("com.trans.mapper.*")
+@Slf4j
 public class AppRunApplication {
     public static void main(String[] args) {
         SpringApplication.run(AppRunApplication.class,args);
+
+        ArrayList<Student> students = new ArrayList<>();
+        int i = 0;
+        while (true){
+            Student student = new Student();
+            student.setAge(i++);
+            student.setName("a");
+            students.add(student);
+            log.debug("student+{}",students);
+        }
     }
 }
