c3109a7ffbd3c4065caec83a683f9bc1fc398425
成都_邹佳骏 <714001035@qq.com>
2022-07-21 15:18:19 +0800

    nacos config 测试

--- transl-test/src/main/java/com/trans/controller/TransTestController.java
+++ transl-test/src/main/java/com/trans/controller/TransTestController.java
@@ -2,20 +2,25 @@ package com.trans.controller;
 
 import com.trans.until.R;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 @RequestMapping("v1/trans")
 public class TransTestController {
 
     @Value("${server.port}")
     private Integer port;
 
+    @Value("${config.test}")
+    private String test;
+
     @GetMapping("/test")
     public R test() throws Exception{
 
-        return R.success(port);
+        return R.success(test);
     }
 }
